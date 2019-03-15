2972af00073f36787bf76e757e94e22f8e777123
qingdi <houzqiang@gmail.com>
2019-03-15 21:44:06 +0800

    init

---
+++ hello.c
@@ -0,0 +1,5 @@
+//
+// Created by houziqiang on 2019-03-15.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,8 @@
+//
+// Created by houziqiang on 2019-03-15.
+//
+
+#ifndef AG_HELLO_H
+#define AG_HELLO_H
+
+#endif //AG_HELLO_H
